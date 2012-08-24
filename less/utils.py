dd4cff67b0c5ef2de13d8b152f4cea3c333213e7
tkliuxing <ouyanghongyu@gmail.com>
2012-08-24 17:50:52 +0800

    修正css中url的替换错误.

--- less/utils.py
+++ less/utils.py
@@ -1,4 +1,4 @@
-import posixpath
+import urlparse
 import re
 import os
 
@@ -11,16 +11,16 @@ class URLConverter(object):
     def __init__(self, content, source_path):
         self.content = content
         self.source_dir = os.path.dirname(source_path)
+        if not self.source_dir.endswith("/"):
+            self.source_dir = self.source_dir + "/"
 
     def convert_url(self, matchobj):
         url = matchobj.group(1)
         url = url.strip(' \'"')
         if url.startswith(('http://', 'https://', '/', 'data:')):
             return "url('%s')" % url
-        full_url = posixpath.normpath("/".join([self.source_dir, url]))
+        full_url = urlparse.urljoin(self.source_dir, url)
         return "url('%s')" % full_url
 
     def convert(self):
         return URL_PATTERN.sub(self.convert_url, self.content)
-
-
